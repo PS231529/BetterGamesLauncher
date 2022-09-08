62b3d638d786b68d4eb11efeba9e0ed68808b5c5
ps231529 <ps231529@summacollege.nl>
2022-09-08 15:31:57 +0200

    niks

--- EindopdrachtPRG3/MainWindow.xaml.cs
+++ EindopdrachtPRG3/MainWindow.xaml.cs
@@ -453,21 +453,16 @@ namespace EindopdrachtPRG3
                 gamens.Style = rsrc;
                 Border border = new Border();
                 TextBlock textBlock = new TextBlock();
-<<<<<<< HEAD
-                textBlock.Text = file.ToString();
+
+                textBlock.Text = map[5];
                 gamens.Children.Add(border);
                 gamens.Children.Add(textBlock);
                 Grid.SetRow(gamens, 0+yindex);
                 Grid.SetColumn(gamens, 0 + xindex);
                 Main.Children.Add(gamens);
                 xindex++;
-=======
-                textBlock.Text = map[5];
-                gamens.Children.Add(textBlock);
-                Grid.SetRow(gamens, 0 + yindex);
-                Grid.SetColumn(gamens, 1 + xindex);
-                Main.Children.Add(gamens);
->>>>>>> 39e1b4da99177f7843f8429aa638504586955f90
+
+
 
                 if (xindex == 5)
                 {
